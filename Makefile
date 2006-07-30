a1c081ab54b03d31fe530514201e18444e02204e
Jan Gerber <j@bootlab.org>
2006-07-30 19:38:12 +0200

    patch by Moritz Grimm <gtgbr@gmx.net>

--- Makefile
+++ Makefile
@@ -1,13 +1,31 @@
-PREFIX=/usr
-INSTALL=install
+PROG =		oggfwd
+SRC =		oggfwd.c
+MAN =		oggfwd.1
 
-all: oggfwd
+PREFIX ?=	/usr/local
+BINDIR ?=	${PREFIX}/bin
+MAN1DIR ?=	${PREFIX}/man/man1
 
-oggfwd: oggfwd.c
-	gcc -Wall -O2 -ffast-math -fsigned-char -lshout -lpthread -logg -lvorbis -ltheora oggfwd.c  -o oggfwd
+CC ?=		gcc
+CFLAGS ?=	-O2 -pipe
+CFLAGS +=	-Wall -ffast-math -fsigned-char
 
-install: oggfwd
-	$(INSTALL) -D -m 755 oggfwd $(PREFIX)/bin/oggfwd		
+INSTALL =	install
+
+PTHREADFLAGS ?=	-pthread
+INCLUDEFLAGS ?=	-I${PREFIX}/include
+LINKFLAGS ?=	-L${PREFIX}/lib
+LINKFLAGS +=    -lshout -logg -lvorbis -ltheora -lspeex
+
+all: ${PROG}
+
+${PROG}: ${SRC}
+	${CC} ${CFLAGS} ${PTHREADFLAGS} ${INCLUDEFLAGS} ${LINKFLAGS} -o ${PROG} $<
+
+install: ${PROG}
+	${INSTALL} -c -m 555 -o root -g bin ${PROG} ${BINDIR}
+	test -d ${MAN1DIR} || ${INSTALL} -d -o root ${MAN1DIR}
+	${INSTALL} -c -m 444 -o root -g bin ${MAN} ${MAN1DIR}
 
 clean:
-	rm -f oggfwd
+	-@rm ${PROG} *~ core *.core
--- oggfwd.c
+++ oggfwd.c
@@ -1,96 +1,206 @@
-/* oggfwd.c: A very useful demonstration of the libshout API, by J <j@v2v.cc> */
-/* some improvements by -- rafael2k <rafael (AT) riseup (DOT) net> */
-/* thanx to rhatto <rhatto (AT) riseup (DOT) net> and others at submidialogia :-P   */
+/*
+ * oggfwd    -- Forward an Ogg stream from stdin to an Icecast server
+ *		A useful demonstration of the libshout API
+ *
+ * This program is distributed under the GNU General Public License, version 2.
+ * A copy of this license is included with this source.
+ *
+ * This program is provided "as-is", with no explicit or implied warranties of
+ * any kind.
+ *
+ * Copyright (C) 2006,		J <j@v2v.cc>,
+ *				rafael2k <rafael(at)riseup(dot)net>,
+ *				Moritz Grimm <gtgbr@gmx.net>
+ */
+/* thanx to rhatto <rhatto (AT) riseup (DOT) net> and others at submidialogia :-P */
 
+#include <sys/types.h>
+#include <sys/param.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#ifndef NO_UNISTD_H
+#  include <unistd.h>
+#endif /* no-NO_UNISTD_H */
+
 #include <shout/shout.h>
 
-int main(int argc, char **argv) {
-	shout_t *shout;
-	unsigned char buff[4096];
-	long read, ret, total;
+extern char    *__progname;
+extern char    *optarg;
+extern int	optind;
+extern int	errno;
 
-	if (!(shout = shout_new())) {
-		printf("Could not allocate shout_t\n");
+#if defined(__dead)
+__dead void
+#else
+void
+#endif /* __dead */
+usage(void)
+{
+	printf("usage: %s "
+	       "[-hp] "
+	       "[-d filename] "
+	       "[-D description] "
+	       "[-g genre] "
+	       "[-n name] "
+	       "[-u URL]\n"
+	       "              address port password mountpoint\n",
+	       __progname);
+	exit(1);
+}
+
+void
+set_argument_string(char **param, char *opt, char optname)
+{
+	size_t siz;
+
+	if (*param) {
+		fprintf(stderr, "%s: Parameter -%c given multiple times\n",
+			__progname, optname);
+		usage();
+	}
+
+	siz = strlen(opt) + 1;
+	if (siz >= MAXPATHLEN) {
+		fprintf(stderr, "%s: Argument for parameter -%c too long\n",
+			__progname, optname);
+		exit(1);
+	}
+
+	if ((*param = malloc(siz)) == NULL) {
+		fprintf(stderr, "%s: %s\n", __progname, strerror(errno));
+		exit(1);
+	}
+
+	snprintf(*param, siz, "%s", opt);
+}
+
+int
+main(int argc, char **argv)
+{
+	shout_t        *shout;
+	unsigned char	buff[4096];
+	int		ret, ch;
+	unsigned int	pFlag;
+	char	       *dump_filename, *description, *genre, *name, *url;
+	size_t		bytes_read, total;
+	unsigned short	port;
+
+	pFlag = 0;
+	dump_filename = description = genre = name = url = NULL;
+	while ((ch = getopt(argc, argv, "d:D:g:hn:pu:")) != -1) {
+		switch (ch) {
+		case 'd':
+			set_argument_string(&dump_filename, optarg, 'd');
+			break;
+		case 'D':
+			set_argument_string(&description, optarg, 'D');
+			break;
+		case 'g':
+			set_argument_string(&genre, optarg, 'g');
+			break;
+		case 'n':
+			set_argument_string(&name, optarg, 'n');
+			break;
+		case 'p':
+			pFlag = 1;
+			break;
+		case 'u':
+			set_argument_string(&url, optarg, 'u');
+			break;
+		case 'h':
+		default:
+			usage();
+		}
+	}
+	argc -= optind;
+	argv += optind;
+
+	if (argc != 4) {
+		fprintf(stderr, "%s: Wrong number of arguments\n", __progname);
+		usage();
+	}
+
+	if ((shout = shout_new()) == NULL) {
+		fprintf(stderr, "%s: Could not allocate shout_t\n",
+			__progname);
+		return (1);
+	}
+
+	if (shout_set_host(shout, argv[0]) != SHOUTERR_SUCCESS) {
+		fprintf(stderr, "%s: Error setting hostname: %s\n", __progname,
+			shout_get_error(shout));
+		return (1);
+	}
+
+	if (sscanf(argv[1], "%hu", &port) != 1) {
+		fprintf(stderr, "Invalid port `%s'\n", argv[1]);
+		usage();
+	}
+	if (shout_set_port(shout, port) != SHOUTERR_SUCCESS) {
+		fprintf(stderr, "%s: Error setting port: %s\n", __progname,
+			shout_get_error(shout));
+		return (1);
+	}
+
+	if (shout_set_password(shout, argv[2]) != SHOUTERR_SUCCESS) {
+		fprintf(stderr, "%s: Error setting password: %s\n", __progname,
+			shout_get_error(shout));
+		return (1);
+	}
+
+	if (shout_set_mount(shout, argv[3]) != SHOUTERR_SUCCESS) {
+		fprintf(stderr, "%s: Error setting mount: %s\n", __progname,
+			shout_get_error(shout));
 		return 1;
 	}
 
-	if(argc != 5 && argc != 11) {
-	  puts("oggfwd\n\n" 
-	       "|| Simple Usage: ||\n" 
-	       " oggfwd server_address port_number password target\n"
-	       "Sample:\n oggfwd icecast.v2v.cc 8000 password /theora.ogg\n"
-	       "\n\n"
-	       "|| Advanced Usage: ||\n"
-	       "Use strings (a single word or a \"w o r d\" with spaces, inside \" \" ) or use the\npossible values available inside the parentheses: -> 0 means False | 1 means True <-\n\n"
-	       "Command Format:\n oggfwd server_address port_number password mount_point radio_name url genre description set_public(0,1) dump_file(0,dump_file_name) \n\n"
-	       "Sample:\n oggfwd orelhao.radiolivre.org 8100 secret_password /mount_point.ogg \"Radio Freak\" http://doidos.radiolivre.org/ free_style \"A Radio dos doid@s\" 1 /tmp/dumpfile.ogg \n\n");
-		exit(-1);
-	}
-
-	if (shout_set_host(shout, argv[1]) != SHOUTERR_SUCCESS) {
-	  printf("Error setting hostname: %s\n", shout_get_error(shout));
-	  return 1;
-	}
-	
-	shout_set_port(shout, atoi(argv[2]));
-	
-	if (shout_set_password(shout, argv[3]) != SHOUTERR_SUCCESS) {
-	  printf("Error setting password: %s\n", shout_get_error(shout));
-	  return 1;
-	}
-	
-	if (shout_set_mount(shout, argv[4]) != SHOUTERR_SUCCESS) {
-	  printf("Error setting mount: %s\n", shout_get_error(shout));
-	  return 1;
-	}
-
 	shout_set_format(shout, SHOUT_FORMAT_VORBIS);
-	
-	/* Advanced options */
-	if (argc == 11) {
-	  
-	  shout_set_name(shout, argv[5]);
 
-	  shout_set_url(shout,argv[6]);
+	shout_set_public(shout, pFlag);
 
-	  shout_set_genre(shout, argv[7]);
+	if (dump_filename)
+		shout_set_dumpfile(shout, dump_filename);
 
-	  shout_set_description(shout, argv[8]);
+	if (description)
+		shout_set_description(shout, description);
 
-	  shout_set_public(shout, atoi(argv[9]));
+	if (genre)
+		shout_set_genre(shout, genre);
 
-	  if (strncmp(argv[10], "0", 1)) 
-	    shout_set_dumpfile(shout, argv[10]);
-	}
+	if (name)
+		shout_set_name(shout, name);
+
+	if (url)
+		shout_set_url(shout, url);
 
 	if (shout_open(shout) == SHOUTERR_SUCCESS) {
-		printf("Connected to server...\n");
+		printf("%s: Connected to server...\n", __progname);
 		total = 0;
-		while (1) {
-			read = fread(buff, 1, sizeof(buff), stdin);
-			total = total + read;
+		for (;;) {
+			bytes_read = fread(buff, 1, sizeof(buff), stdin);
+			total += bytes_read;
 
-			if (read > 0) {
-				ret = shout_send(shout, buff, read);
+			if (bytes_read > 0) {
+				ret = shout_send(shout, buff, bytes_read);
 				if (ret != SHOUTERR_SUCCESS) {
-					printf("DEBUG: Send error: %s\n", shout_get_error(shout));
+					printf("%s: Send error: %s\n",
+					       __progname,
+					       shout_get_error(shout));
 					break;
 				}
-			} else {
+			} else
 				break;
-			}
 
 			shout_sync(shout);
 		}
-	} else {
-		printf("Error connecting: %s\n", shout_get_error(shout));
-	}
+	} else
+		printf("%s: Error connecting: %s\n", __progname,
+		       shout_get_error(shout));
 
 	shout_close(shout);
 
-	return 0;
+	return (0);
 }
